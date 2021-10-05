19333b0880f5223b410932f678301c15e1da7a7d
rubab <rubabume840@gmail.com>
2021-10-05 19:12:33 +0500

    customizations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   // {path:'salesorders',loadChildren: () => import('./modules/salesorders/salesorders.module').then(m => m.SalesordersModule)},
   {path:'home',loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
+  {path: 'main', component: MainComponent},
   {path:'',loadChildren: () => import('./modules/default/default.module').then(m => m.DefaultModule)},
 ];
 
